ea7a10766fe1f5eaa1b800d418fa51cc6db24f1c
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-06-20 21:08:13 +0530

    /artwork view verifies input file as Image

--- artwork/api/views.py
+++ artwork/api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from rest_framework.decorators import api_view, permission_classes
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.exceptions import ParseError
 from rest_framework.response import Response
 
-# Create your views here.
+from PIL import Image
 
 @api_view(('GET',))
 def index(request):
@@ -19,3 +20,24 @@ def create_artwork(request):
 		template = loader.get_template('index.html')
 		context = {}
 		return HttpResponse(template.render(context, request))
+
+	if request.method == 'POST':
+
+		if 'file' not in request.data:
+			raise ParseError("Empty content")
+
+		f = request.data['file']
+
+		try:
+			img = Image.open(f)
+			img.verify()
+		except:
+			raise ParseError("Unsupported image type")
+
+
+
+		template = loader.get_template('index.html')
+		context = {}
+		return HttpResponse(template.render(context, request))
+
+
