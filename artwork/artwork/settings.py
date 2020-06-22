34b391808798198443582bdc14c8f3e48e0cf90c
Raunaq <raunaq.soni+gurraunaqsingh@gmail.com>
2020-06-22 14:58:42 +0530

    Modified settings.py. Changed val of variable to use env vars

--- artwork/artwork/settings.py
+++ artwork/artwork/settings.py
@@ -24,12 +24,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pgp(p#ftb11r&tbnzo!uv(z!s2)%ssu60z*8h30-q8lvb@zy^@'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
